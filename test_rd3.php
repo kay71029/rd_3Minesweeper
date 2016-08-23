58ac435905f20ab7362682a44fbdb20721d8ae3a
kay71029 <kay71029@gmail.com>
2016-08-23 15:36:58 +0800

    Add files via upload

--- test_rd3.php
+++ test_rd3.php
@@ -9,36 +9,39 @@ header('Content-Type: text/html; charset=utf-8');
     
     $id  = $_GET["map"];
     $idLength = strlen( $id );
+    $ans = 1;
+    
     //var_dump($idLength);
      
     //檢查長度
-     if( $idLength > $length-1) {
+     if ($idLength > $length-1) {
          
-        echo "不符合 長度太長";
+        echo "不符合 長度太長 長度為:$idLength";
+        exit();
         
-     }else if($idLength < $length-1) {
+     } else if ($idLength < $length-1) {
          
-        echo "不符合 長度太短";
+        echo "不符合 長度太短 長度為:$idLength";
+        exit();
         
-     }else if($idLength = $length-1) {
         
+     } else if ($idLength = $length-1) {
         //echo "正確";
      } 
-    
     
     //檢查炸彈
     $bombTest = substr_count($id, "M");
     
-    if( $bombTest > $bomb) {
+    if ( $bombTest > $bomb) {
+        echo "不符合 炸彈數太多 數量為:$bombTest";
+        exit();
+        
+     } else if($bombTest < $bomb) {
          
-        echo "不符合 炸彈數太多";
+        echo "不符合 炸彈數太少 數量為:$bombTest";
+        exit();
         
-     }else if($bombTest < $bomb) {
-         
-        echo "不符合 炸彈數太少";
-        
-     }else if($bombTest = $bomb) {
-        
+     } else if($bombTest = $bomb) {
         //echo "正確";
      }
      
@@ -52,66 +55,54 @@ header('Content-Type: text/html; charset=utf-8');
         for($y = 0; $y < 10; $y++) {
             $place[$x][$y] = $newId [$i];
             $i++;
-            
         }
     }
-    //var_dump($place);
 
     for($x = 0; $x < 10; $x++) {
-        for($j = 0; $j < 10; $j++) {
-            if($place[$x][$j] !=="0") {
+        for($y = 0; $y < 10; $y++) {
+            if($place[$x][$y] !=="M") {
                 $num = 0;
                 //左上
-                if($place[$x-1][$j-1] === "M") {
+                if($place[$x-1][$y-1] === "M") {
                     $num++;
                 }
                 //上
-                if($place[$x-1][$j] === "M") {
+                if($place[$x-1][$y] === "M") {
                     $num++;
                 }
                 //右上
-                if($place[$x-1][$j+1] === "M") {
+                if($place[$x-1][$y+1] === "M") {
                     $num++;
                 }
                 //左
-                if($place[$x][$j-1] === "M") {
+                if($place[$x][$y-1] === "M") {
                     $num++;
                 }
                 //右
-                if($place[$x][$j+1] === "M") {
+                if($place[$x][$y+1] === "M") {
                     $num++;
                 }
                 //左下
-                if($place[$x+1][$j-1] === "M") {
+                if($place[$x+1][$y-1] === "M") {
                     $num++;
                 }
                 //下
-                if($place[$x+1][$j] === "M") {
+                if($place[$x+1][$y] === "M") {
                     $num++;
                 }
                 //右下
-                if($place[$x+1][$j+1] === "M"){
+                if($place[$x+1][$y+1] === "M"){
                     $num++;
                 }
-                //$place[$x][$j] = $num;
-                if( $num != $place[$x][$j]){
-                    echo "不符合 ";
+                //$place[$x][$y] = $num;
+                if($num != $place[$x][$y]){
+                    echo "位置在[".$x."][".$y."]不符合 ";
+                    $ans = 0;
                     break 2;
-                    
-                }
+                } 
             }
         }
     }
-   
-    // for($x = 0; $x < 10; $x++) {
-    //     for($y = 0; $y < 10; $y++) {
-    //         $data = $data.$place[$x][$y];
-    //         if($y === 9) {
-    //             if($x != 9) {
-    //                 $data = $data."N";
-    //             }
-    //         }
-    //     }
-         
-    // }
-    // echo $data;
+     if ($ans == 1) {
+        echo "符合 ";
+     }
