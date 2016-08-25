9e4d8e2a2fc65843c03ee60115b218611e7c25a2
kay71029 <kay71029@gmail.com>
2016-08-25 18:14:33 +0800

    Add files via upload

---
+++ CheckId.php
@@ -0,0 +1,25 @@
+<?php
+
+header('Content-Type: text/html; charset=utf-8');
+require('db.php');
+
+    $db = new Database();
+    
+    $id = $_GET["id"];
+    $number = $_GET["number"];
+
+    
+    
+    $sql = "SELECT `number` FROM `record` WHERE `number`= $number";
+    $record = $db->select($sql);
+ 
+     if($record[0]['number'] == null ){
+        
+        echo json_encode(array('massage' => "失敗"),JSON_UNESCAPED_UNICODE); 
+     }
+     if($record[0]['number'] != null) {
+         
+        echo json_encode(array('id' => $id, 'number' => $number,'massage' => "成功"),JSON_UNESCAPED_UNICODE); 
+     }
+    
+ 
