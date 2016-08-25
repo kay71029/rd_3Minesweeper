bd841728470b842528c9639639567b06505d351c
kay71029 <kay71029@gmail.com>
2016-08-26 00:57:16 +0800

    Add files via upload

--- rd3_1.php
+++ rd3_1.php
@@ -1,22 +1,28 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
 $time_start = microtime(true); 
+    
     //畫出陣列
     for($x = 0; $x < 50; $x++) {
         for($y = 0; $y < 60; $y++) {
             $place[$x][$y] = "0";
         }
     }
+    
     //產生炸彈
     $Rand = Array();
-    for($i = 0; $i < 1200; $i++) {
-        $m = rand(0,2999);
-        if (in_array($m , $Rand)){
-            $i--;
-            // continue 2;
+    $gonb = 1200;
+    $a = 0;
+    while( !$Rand[$a] ){
+        if( $a > $gonb-1 ) break;
+        $sn = mt_rand(1,3000); 
+        if(!in_array($sn,$Rand)){
+           
+            $Rand[$a] = $sn;
+            $a++;
         }
-        $Rand[] = $m;
     }
+ //var_dump($Rand);
      for($i = 0; $i < 1200; $i++) {
         $temp = $Rand[$i];
         $x = floor($temp/50);
@@ -25,58 +31,56 @@ $time_start = microtime(true);
     }
     
     for($x = 0; $x < 50; $x++) {
-        for($j = 0; $j < 60; $j++) {
-            if($place[$x][$j] === "0") {
+        for($y = 0; $y < 60; $y++) {
+            if($place[$x][$y] === "0") {
                 $num = 0;
                 //左上
-                if($place[$x-1][$j-1] === "M") {
+                if($place[$x-1][$y-1] === "M") {
                     $num++;
                 }
                 //上
-                if($place[$x-1][$j] === "M") {
+                if($place[$x-1][$y] === "M") {
                     $num++;
                 }
                 //右上
-                if($place[$x-1][$j+1] === "M") {
+                if($place[$x-1][$y+1] === "M") {
                     $num++;
                 }
                 //左
-                if($place[$x][$j-1] === "M") {
+                if($place[$x][$y-1] === "M") {
                     $num++;
                 }
                 //右
-                if($place[$x][$j+1] === "M") {
+                if($place[$x][$y+1] === "M") {
                     $num++;
                 }
                 //左下
-                if($place[$x+1][$j-1] === "M") {
+                if($place[$x+1][$y-1] === "M") {
                     $num++;
                 }
                 //下
-                if($place[$x+1][$j] === "M") {
+                if($place[$x+1][$y] === "M") {
                     $num++;
                 }
                 //右下
-                if($place[$x+1][$j+1] === "M"){
+                if($place[$x+1][$y+1] === "M"){
                     $num++;
                 }
-                $place[$x][$j] = $num;
+                $place[$x][$y] = $num;
             }
         }
     }
-   
+   //var_dump($place);
     for($x = 0; $x < 50; $x++) {
         for($y = 0; $y < 60; $y++) {
             $data = $data.$place[$x][$y];
-            if($y === 59) {
+            if($y == 59) {
                 if($x != 49) {
                     $data = $data."N";
                 }
             }
         }
-         
     }
-
     echo $data;
  $end = microtime(true); 
- //echo "<br>" .($end - $time_start);
+ echo "<br>" .($end - $time_start);
